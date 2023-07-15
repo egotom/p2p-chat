11da7cdd04f0b08806ce86c4c0e8efea8f7da8ee
tingchang <12547762+tingchang@user.noreply.gitee.com>
2023-07-15 14:03:52 +0800

    f

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,23 @@
 import {supabase} from '@/components/supabase'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {Spin} from '@/assets/icons'
 import {useUser} from '@/components/userCtx'
 
 type Props = {}
 
 export default function Login({}:Props){
-    const {setUser} = useUser()
+    const {setUser,user} = useUser()
     const [lg, setLg] = useState<any>({email:'', password:'', hidden:false, loadding:false })
     const [rg, setRg] = useState<any>({email:'', password:'', repass:'', nickname:'', loadding:false})
     const [nt, setNt] = useState<any>({})
+    const [coll, setColl] = useState<boolean>(false)
+    const [act, setact] = useState<string>('lg')
+
+    useEffect(()=>{
+        if(!user?.coll) return
+        setColl(user.coll)
+    },[user])
+
     function signin(){
         const ers:any ={}
         if(lg.email?.trim().length < 6)
@@ -31,7 +39,7 @@ export default function Login({}:Props){
                     return
                 }
                 // console.log(res.data)
-                setUser(res.data.user)
+                setUser({...user, ...res.data.user})
 
             }).catch((e:any)=>{
                 // console.log(e.error.message)
@@ -86,15 +94,16 @@ export default function Login({}:Props){
     }
 return (
 <div className='h-full w-full flex justify-around items-center rounded'>
-    <div className='basis-1/2 h-full flex flex-col bg-local rounded-l' style={{backgroundImage: "url(/bg-register.jpg)"}}>
-        <div className="my-6 text-lg self-end px-3 text-white">登录</div>
+    <div className={act==='lg'?coll?'w-full h-full flex flex-col items-center justify-center':'basis-1/2 h-full flex flex-col rounded-l':'hidden'} 
+        style={{backgroundImage: "url(/bg-register.jpg)", backgroundSize:"cover"}}>
+        <div className={coll?"my-6 text-lg self-center px-3 text-white":"my-6 text-lg self-end px-3 text-white"}>登录</div>
         <div className="text-red-600 text-sm h-8 self-center w-64 mb-3">{nt.lres}</div>
-        <div className="flex itemts-center gap-3 my-2">
+        <div className="flex items-center gap-3 my-2">
             <div className='text-white text-sm w-10 ml-10'>账号</div>
             <input type="text" className="border border-gray-400 outline-none text-sm p-1"
                 name="email" value={lg.email} onChange={hChangeLg}/>
         </div>
-        <div className="flex itemts-center gap-3">
+        <div className="flex items-center gap-3">
             <div className='text-white text-sm w-10 ml-10'></div>
             <div className="text-red-600 text-sm h-7 self-start ">{nt.lemail}</div>
         </div>
@@ -103,7 +112,7 @@ return (
             <input type="password" className="border border-gray-400 outline-none text-sm p-1"
                 name="password" value={lg.password} onChange={hChangeLg}/>
         </div>
-        <div className="flex itemts-center gap-3">
+        <div className="flex items-center gap-3">
             <div className='text-white text-sm w-10 ml-10'></div>
             <div className="text-red-600 text-sm h-7 self-start ">{nt.lpass}</div>
         </div>
@@ -112,53 +121,55 @@ return (
             <input type="checkbox" className="border border-gray-400 text-sm p-1"
                 name="hidden" value={lg.hidden} onChange={hChangeLg}/>
         </div>
-        <div className="flex itemts-center justify-center gap-3 my-2">
+        <div className="flex items-center justify-center gap-3 my-2">
             <button className='btn' onClick={signin}>
                 {lg.loadding?<Spin css="w-5 h-5 animate-spin"/>:"登录"}
             </button>
+            {coll && <div className="text-white cursor-pointer" onClick={()=>setact('rg')}>注册新用户</div>}
         </div>
     </div>
-    <div className='basis-1/2 h-full flex flex-col items-center'>
-        <div className="my-6 text-lg self-start px-3 ">注册新用户</div>
+    <div className={act==='rg'?coll?'w-full h-full flex flex-col items-center justify-center':'basis-1/2 h-full flex flex-col items-center':'hidden'}
+        style={{backgroundImage: "url(/bg-register.jpg)", backgroundSize:"cover"}}>
+        <div className={coll?"my-6 text-lg self-center px-3 text-white":"my-6 text-lg self-start px-3 "}>注册新用户</div>
         <div className="flex flex-col">
             <div className="text-red-700 text-sm h-8">{nt.rres}</div>
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end">邮箱</div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}>邮箱</div>
                 <input type="text" className="border border-gray-400 text-sm p-1" 
                     name="email" value={rg.email} onChange={hChangeRg} />
             </div>
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end"></div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}></div>
                 <div className="text-red-700 text-sm h-7 self-start ">{nt.remail}</div>
             </div>
 
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end">昵称</div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}>昵称</div>
                 <input type="text" className="border border-gray-400 text-sm p-1" 
                     name="nickname" value={rg.nickname} onChange={hChangeRg}/>
             </div>
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end"></div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}></div>
                 <div className="text-red-700 text-sm h-7 flex justify-start">{nt.rnick}</div>
             </div>
 
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end">密码</div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}>密码</div>
                 <input type="password" className="border border-gray-400 text-sm p-1" 
                     name="password" value={rg.password} onChange={hChangeRg}/>
             </div>
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end"></div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}></div>
                 <div className="text-red-700 text-sm h-7 flex justify-start">{nt.rpass}</div>
             </div>
 
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end">确认密码</div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}>确认密码</div>
                 <input type="password" className="border border-gray-400 text-sm p-1" 
                     name="repass" value={rg.repass} onChange={hChangeRg}/>
             </div>
             <div className="flex items-center">
-                <div className="text-sm mr-3 w-14 flex justify-end"></div>
+                <div className={coll?"text-sm mr-3 w-14 flex justify-end text-white":"text-sm mr-3 w-14 flex justify-end"}></div>
                 <div className="text-red-700 text-sm h-7 flex justify-start">{nt.rrepass}</div>
             </div>
             <div className="flex items-center">
@@ -166,9 +177,13 @@ return (
                 <div className="text-green-700 text-sm h-7 flex justify-start">{nt.rsuccess}</div>
             </div>
         </div>
-        <button className='btn'onClick={signUp}>
-            {rg.loadding?<Spin css="w-5 h-5 animate-spin"/>:"注册"}
-        </button>
+        <div className="flex items-center gap-3">
+            <button className='btn'onClick={signUp}>
+                {rg.loadding?<Spin css="w-5 h-5 animate-spin"/>:"注册"}
+            </button>
+            {coll && <div className="text-white cursor-pointer" onClick={()=>setact('lg')}>登录账号</div>}
+        </div>
+        
     </div>
 </div>    
 )}
